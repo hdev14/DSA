1dc2ffc600eb59aeb113909a9f5bb8be8e92bae3
Hermerson Araújo <ne.hermerson@gmail.com>
2025-02-28 14:16:43 -0300

    finiish knuth shuffle

--- algorithms/knuth_shuffle.c
+++ algorithms/knuth_shuffle.c
@@ -1,4 +1,6 @@
-
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 void exchange(int *array, int first_idx, int second_idx)
 {
@@ -11,12 +13,23 @@ void shuffle(int *array, int length)
 {
     for (int idx = 0; idx < length; idx++)
     {
-        int random_idx = 0; // TODO use rand()
+        int random_idx = rand() % (idx + 1);
         exchange(array, idx, random_idx);
     }
 }
 
 int main()
 {
+    int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    shuffle(array, 9);
+
+    for (int i = 0; i < 9; i++)
+    {
+        printf("%i ", array[i]);
+    }
+
+    printf("\n");
+
     return 0;
 }
